ffba34926d5f7f98b97ebc7541b58c22275584cc
Adama Diallo <adamadial58@me.com>
2018-06-06 10:43:02 +0400

    creation de la route /Liste

--- app.js
+++ app.js
@@ -8,14 +8,21 @@
 // server.listen(5000, function() { console.log("Listening on port 5000")});
 
 
-
-
-
 var express = require('express');
 var server = express();
+
 server.get('/', function(request, response) {
 	response.sendFile(__dirname+'/index.html');
 });
+server.get('/liste', function(request, response) {
+	response.sendFile(__dirname+'/data.json');
+});
+
 server.listen(3020, function() { console.log("Listening on port 3020")});
 
+
+
+// app.get('/liste', function(req, res) {
+//     res.sendFile(__dirname+'/data.json');
+//   });
 
